1a532427c6a68d4ccc56df01d41df93580feaf24
Wes Hinsley <w.hinsley@imperial.ac.uk>
2020-05-15 16:58:40 +0100

    Produce ASCII txt for Isle of Wight

--- jobs/e18_wpop_adm2/create_adm2.java
+++ jobs/e18_wpop_adm2/create_adm2.java
@@ -1,9 +1,15 @@
 package jobs.e18_wpop_adm2;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+
+import javax.imageio.ImageIO;
 
 import com.mrc.GlobalRasterTools.GlobalRasterTools;
 
@@ -12,14 +18,12 @@ public class create_adm2 {
   private final static String out_path = "E:/Jobs/WPop_ADM2/";
   private final static String wpop_file = "E:/Data/Census/WorldPop/global/worldpop2020global.bil";
   
-  private final static int max_level = 1;
+  private final static int max_level = 2;
   private final static String[] popa_countries = new String[] { "United Kingdom"};
   private final static String[] gadm36_countries = new String[] { "United Kingdom"};
   private final static String index_file_out = "wpop_uk_adm"+max_level+"_index.txt";
   private final static String pop_file_out = "wpop_uk_adm"+max_level+".txt";
  
-  
-  
   static void update_ids(String[] name_bits, String[] num_bits, String[] current_admin, int[] admin_no) {
     for (int lev = 0; lev < max_level; lev++) {
       if (!current_admin[lev].equals(num_bits[lev])) {
@@ -122,8 +126,105 @@ public class create_adm2 {
     PW.close();
   }
   
+  public void createIsleOfWightAscii() throws Exception {
+    BufferedReader br = new BufferedReader(new FileReader(out_path + "wpop_uk_adm2_index.txt"));
+    String s = br.readLine();
+    String admin="";
+    while (s!=null) {
+      String[] bits = s.split("\t");
+      if (bits[3].equals("Isle of Wight")) {
+        admin = bits[0];
+        break;
+      }
+      s = br.readLine();
+    }
+    br.close();
+    br = new BufferedReader(new FileReader(out_path + "wpop_uk_adm2.txt"));
+    s = br.readLine();
+    int minx = Integer.MAX_VALUE;
+    int maxx = Integer.MIN_VALUE;
+    int miny = Integer.MAX_VALUE;
+    int maxy = Integer.MIN_VALUE;
+    double minlon = Double.MAX_VALUE;
+    double minlat = Double.MAX_VALUE;
+    
+    System.out.println("Finding bounds");
+    while (s!=null) {
+      String[] bits = s.split("\t");
+      if (bits[4].equals(admin)) {
+        double lon = Double.parseDouble(bits[0]);
+        double lat = Double.parseDouble(bits[1]);
+        minlat = Math.min(minlat,  lat);
+        minlon = Math.min(minlon,  lon);
+        int lon_cell = 21600 + (int) Math.round(lon * 120.0);
+        int lat_cell = 10800 - (int) Math.round(lat * 120.0);
+        minx = Math.min(minx,  lon_cell);
+        maxx = Math.max(maxx,  lon_cell);
+        miny = Math.min(miny,  lat_cell);
+        maxy = Math.max(maxy,  lat_cell);
+      }
+      s = br.readLine();
+    }
+    
+    br.close();
+    System.out.println("Bounds: x: "+minx+" .. "+maxx+", y: "+miny+" .. "+maxy);
+    
+    int[][] grid = new int[1+(maxy-miny)][1+(maxx-minx)];
+    for (int j=0; j<grid.length; j++)
+      for (int i=0; i<grid[j].length; i++)
+        grid[j][i]=-999;
+    int max_pop=0;
+    int total_pop=0;
+    br = new BufferedReader(new FileReader(out_path + "wpop_uk_adm2.txt"));
+    s = br.readLine();
+    while (s!=null) {
+      String[] bits = s.split("\t");
+      if (bits[4].equals(admin)) {
+        int pop = Integer.parseInt(bits[2]);
+        max_pop = Math.max(pop,  max_pop);
+        total_pop += pop;
+        double lon = Double.parseDouble(bits[0]);
+        double lat = Double.parseDouble(bits[1]);
+        int lon_cell = 21600 + (int) Math.round(lon * 120.0);
+        int lat_cell = 10800 - (int) Math.round(lat * 120.0);
+        grid[lat_cell-miny][lon_cell-minx] = pop;
+      }
+      s = br.readLine();
+    }
+    
+    br.close();
+    System.out.println("Total population = "+total_pop);
+    int[] cols = new int[256];
+    for (int i=0; i<256; i++) cols[i]=new Color(i,i,i).getRGB();
+    int blue = new Color(0,0,128).getRGB();
+    BufferedImage bi = new BufferedImage(grid[0].length+1, grid.length+1, BufferedImage.TYPE_3BYTE_BGR);
+    for (int j=0; j<grid.length; j++)
+      for (int i=0; i<grid[j].length; i++)
+        if (grid[j][i]<0) bi.setRGB(i, j, blue);
+        else bi.setRGB(i,  j,  cols[(int) (255.0*(grid[j][i]/(double)max_pop))]);
+    ImageIO.write(bi,  "PNG", new File(out_path+ "iow.png"));
+    
+    PrintWriter PW = new PrintWriter(new File(out_path + "iow_ascii.txt"));
+    PW.println("NCOLS     "+grid[0].length);
+    PW.println("NROWS     "+grid.length);
+    PW.println("CELLSIZE  0.00833333333333333333");
+    PW.println("XLLCORNER "+minlon);
+    PW.println("YLLCORNER "+minlat);
+    PW.println("NODATA    -9999");
+    for (int j=0; j<grid.length; j++) {
+      for (int i=0; i<grid[j].length; i++) {
+        if (grid[j][i]<0) PW.print("0");
+        else PW.print(grid[j][i]);
+        if (i<grid[j].length-1) PW.print(" ");
+      }
+      PW.println();
+    }
+    PW.close();
+  }
+       
   public static void main(String[] args) throws Exception {
     create_adm2 CAD2 = new create_adm2();
     CAD2.run();
+    CAD2.createIsleOfWightAscii();
   }
 }
